ffd35b4759107c3489d4f0c666b0b44924cc9665
Basit Ali <cool.saa733@gmail.com>
2022-09-14 16:19:43 +0500

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './App.css';
 
 const App = () => {
   return (
     <div>
-        <h1>Hello</h1>
+        <h1 className='text-3xl font-bold underline '>Hello</h1>
     </div>
   )
 }
