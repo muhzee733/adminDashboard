be57c6b45748ae964da18850e3fa2150347b2a65
Basit Ali <cool.saa733@gmail.com>
2022-09-15 10:17:58 +0500

    Basic Setup plus designing

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Buuton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Buuton
---
+++ src/components/Charts/LineChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineChat = () => {
+  return (
+    <div>LineChat</div>
+  )
+}
+
+export default LineChat
